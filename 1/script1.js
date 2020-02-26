892f0ae54b8f55da3a10da7e23846e38ff12d57f
dobryimolodez <vyacheslav4960@gmail.com>
2020-02-26 08:48:23 +0300

    lesson 1 almost done

---
+++ 1/script1.js
@@ -0,0 +1,23 @@
+//window.onload = function() {
+//};
+var b = document.createElement("button");
+document.body.appendChild(b);
+b.setAttribute("enabled","true");
+b.innerText="eta knopka dolzhna byut ";
+b.setAttribute("style", "width:200px; height:100px; border-radius:20px;");
+b.onclick = function() {
+    alert('matrix has you. knok knok Neo');
+    console.log(b);
+    };
+let d = document.getElementById("kartinka");
+let f = document.getElementById("roditel");
+console.log ("tekst iz consoli");
+//window.onload = function () {setTimeout(onLoadScript, "500")}
+//let garbage = f.removeChild(d);
+//let throwawayNode = f.removeChild(d);
+//document.addEventListener("load", ready);
+ //   this.msContentScript;
+//};
+//function ready() {
+//    load;
+//    };
