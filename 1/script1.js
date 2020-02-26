6a078ab0a8bb7352a6b5c6d00bc42312439d338f
Barcius <dengornik@gmail.com>
2020-02-26 09:26:10 +0300

    pravki

--- 1/script1.js
+++ 1/script1.js
@@ -5,12 +5,13 @@ document.body.appendChild(b);
 b.setAttribute("enabled","true");
 b.innerText="eta knopka dolzhna byut ";
 b.setAttribute("style", "width:200px; height:100px; border-radius:20px;");
-b.onclick = function() {
+b.style.borderRadius = '20px';
+b.onclick = function(hui) {
     alert('matrix has you. knok knok Neo');
-    console.log(b);
+    console.log(hui);
     };
 let d = document.getElementById("kartinka");
-let f = document.getElementById("roditel");
+d.parentElement.removeChild(d);
 console.log ("tekst iz consoli");
 //window.onload = function () {setTimeout(onLoadScript, "500")}
 //let garbage = f.removeChild(d);
