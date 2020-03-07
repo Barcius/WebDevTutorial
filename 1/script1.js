9272978c743838ea494d2307276c5d4120c15ca9
dobryimolodez <vyacheslav4960@gmail.com>
2020-03-07 08:25:30 +0300

    for 2 webdev

--- 1/script1.js
+++ 1/script1.js
@@ -1,24 +1,14 @@
-//window.onload = function() {
-//};
-var b = document.createElement("button");
-document.body.appendChild(b);
-b.setAttribute("enabled","true");
-b.innerText="eta knopka dolzhna byut ";
-b.setAttribute("style", "width:200px; height:100px; border-radius:20px;");
-b.style.borderRadius = '20px';
-b.onclick = function(hui) {
+window.onload = function() {
+    var b = document.createElement("button");
+    document.body.appendChild(b);
+    //b.setAttribute("disabled","true");
+    b.innerText="eta knopka dolzhna byut ";
+    b.setAttribute("style", "width:200px; height:100px; border-radius:20px;");
+    b.style.borderRadius = '30px';
+    b.onclick = function(hui) { 
     alert('matrix has you. knok knok Neo');
-    console.log(hui);
+    console.log(hui);}
+//var pic = document.body.querySelector("img");
+//delete.pic;
+console.log ("tekst iz consoli"); 
     };
-let d = document.getElementById("kartinka");
-d.parentElement.removeChild(d);
-console.log ("tekst iz consoli");
-//window.onload = function () {setTimeout(onLoadScript, "500")}
-//let garbage = f.removeChild(d);
-//let throwawayNode = f.removeChild(d);
-//document.addEventListener("load", ready);
- //   this.msContentScript;
-//};
-//function ready() {
-//    load;
-//    };
