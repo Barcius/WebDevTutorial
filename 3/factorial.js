80d2bbd17cb6f9343efb68a27b942a2bb3e52d36
barcius <dengornik@gmail.com>
2020-04-11 08:40:56 +0300

    greeting in html

--- 3/factorial.js
+++ 3/factorial.js
@@ -1,15 +1,15 @@
 function j(event){
     var gettingNum = document.getElementById('factor');
     var outputRes = document.getElementById('areaRes2');
-    if (event.keyCode === 13) {
-        let res = 1;
-        for (let i = 2; i <= +gettingNum.value; i++) {
-            res = res * i;
-        }
+    // if (event.keyCode === 13) {
+    //     let res = 1;
+    //     for (let i = 2; i <= +gettingNum.value; i++) {
+    //         res = res * i;
+    //     }
         
-        console.log(res);
-        outputRes.innerText = res;
-    }
+    //     console.log(res);
+    //     outputRes.innerText = res;
+    // }
 }
 gettingNum.addEventListener('keydown' , j)
  
--- 3/greeting.js
+++ 3/greeting.js
@@ -3,3 +3,8 @@ const middlename="Андреевич"
 function greet() {
     return 'здрасьте' + name + middlename + ' ,ужасного вам дня!'
 }
+
+// document.onload = function() {
+    const div = document.getElementById('greet-field')
+    div.innerText = greet()
+// };
